70193886a83b777169adf53723c5777558d655ba
YM-IT <linzhangf@dingtalk.com>
2021-03-15 10:50:23 +0800

    在temp文件夹创建了test2类

---
+++ 微博App11-15/Classes/temp/ZFTest2.h
@@ -0,0 +1,17 @@
+//
+//  ZFTest2.h
+//  微博App11-15
+//
+//  Created by 林漳峰 on 2021/3/15.
+//  Copyright © 2021 林漳峰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZFTest2 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 微博App11-15/Classes/temp/ZFTest2.m
@@ -0,0 +1,13 @@
+//
+//  ZFTest2.m
+//  微博App11-15
+//
+//  Created by 林漳峰 on 2021/3/15.
+//  Copyright © 2021 林漳峰. All rights reserved.
+//
+
+#import "ZFTest2.h"
+
+@implementation ZFTest2
+
+@end
